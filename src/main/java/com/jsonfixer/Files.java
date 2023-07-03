a59fab19dd9df09eccc825084b69670bf27a8273
DJFancyAl <abluemleiv@gmail.com>
2023-07-03 10:46:12 -0400

    Results are now displayed after file converted.

--- src/main/java/com/jsonfixer/Files.java
+++ src/main/java/com/jsonfixer/Files.java
@@ -1,36 +1,83 @@
 package com.jsonfixer;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.stream.Collectors;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Files {
 
-    public static void readFile(String filePath) {
-        try (FileReader reader = new FileReader(filePath)) {
+    private static JsonObject readFile(String filePath) {
+        // Read the JSON File
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             // Read the JSON file contents into a string
-            StringBuilder stringBuilder = new StringBuilder();
-            int character;
-            while ((character = reader.read()) != -1) {
-                stringBuilder.append((char) character);
-            }
+            String jsonString = reader.lines().collect(Collectors.joining());
 
             Gson gson = new Gson();
-            String jsonString = stringBuilder.toString();
             JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
-
-            System.out.print(jsonObject);
+            return jsonObject;
         } catch (IOException e) {
             e.printStackTrace();
+            JsonObject emptyObject = new JsonObject();
+            emptyObject.addProperty("message", "Something went wrong.");
+            return emptyObject;
         }
-
     }
 
-    public static void main(String[] args) {
-        System.out.println("Reading...");
-        readFile("C:\\Users\\ablue\\Desktop\\Scripts\\For_Aaron\\JSON Converter\\Files\\151.json");
+    private static JsonObject editFile(JsonObject jsonFile) {
+        // Make Changes
+        jsonFile.addProperty("description", "234 Servers Mining Crypto");
+        jsonFile.remove("seller_fee_basis_points");
+        jsonFile.remove("collection");
+
+        JsonObject properties = jsonFile.getAsJsonObject("properties");
+        properties.remove("creators");
+
+        return jsonFile;
     }
+
+    private static String createFile(JsonObject jsonFile, String filePath) {
+        String regex = ".*\\\\(\\w+).json";
+
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(filePath);
+
+        if (matcher.find()) {
+            filePath = matcher.group(1);
+            try (FileWriter fileWriter = new FileWriter(filePath + ".json")) {
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                String jsonString = gson.toJson(jsonFile);
+                fileWriter.write(jsonString);
+                return jsonString;
+            } catch (IOException e) {
+                e.printStackTrace();
+                ;
+            }
+        }
+        return "Something went wrong.";
+    }
+
+    public String changeOne(String filePath) {
+        JsonObject beforeFile = readFile(filePath);
+        JsonObject afterFile = editFile(beforeFile);
+        String createdFile = createFile(afterFile, filePath);
+        return createdFile;
+    }
+
+    // public static void main(String[] args) {
+    // String filePath = "C:\\Users\\ablue\\Desktop\\Scripts\\For_Aaron\\JSON
+    // Converter\\Files\\before.json";
+    // JsonObject beforeFile = readFile(filePath);
+    // JsonObject afterFile = editFile(beforeFile);
+    // createFile(afterFile, filePath);
+    // }
 
 }
--- src/main/java/com/jsonfixer/PrimaryController.java
+++ src/main/java/com/jsonfixer/PrimaryController.java
@@ -6,12 +6,58 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import com.jsonfixer.Files;
 
 public class PrimaryController {
+    Files fileManager = new Files();
+    TextArea resultText = new TextArea();
+
     @FXML
     private TextField fileInput;
+
+    @FXML
+    private VBox mainLayout;
+
+    private ScrollPane createResultPane() {
+        // Create a scroll pane.
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setId("resultsPane");
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPrefHeight(200.0);
+        scrollPane.setPrefWidth(800.0);
+
+        // Create a text area.
+        resultText.setId("resultsText");
+        resultText.setEditable(false);
+
+        // Add the text area to the scroll pane.
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.getChildren().add(resultText);
+        AnchorPane.setTopAnchor(resultText, 0.0);
+        AnchorPane.setBottomAnchor(resultText, 0.0);
+        AnchorPane.setLeftAnchor(resultText, 0.0);
+        AnchorPane.setRightAnchor(resultText, 0.0);
+        scrollPane.setContent(anchorPane);
+
+        // Anchor the scroll pane to the edges of the main layout.
+        AnchorPane.setTopAnchor(scrollPane, 0.0);
+        AnchorPane.setBottomAnchor(scrollPane, 0.0);
+        AnchorPane.setLeftAnchor(scrollPane, 0.0);
+        AnchorPane.setRightAnchor(scrollPane, 0.0);
+
+        // Create a new AnchorPane to hold the scroll pane.
+        AnchorPane resultPane = new AnchorPane();
+        resultPane.getChildren().add(scrollPane);
+
+        return scrollPane;
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
@@ -36,6 +82,11 @@ public class PrimaryController {
             System.out.println("Insert some text.");
         } else {
             System.out.println("Converting file " + filePath + "...");
+            String updatedJSON = fileManager.changeOne(filePath);
+            ScrollPane resultPane = createResultPane();
+            mainLayout.getChildren().add(resultPane);
+            resultText.setText(updatedJSON);
+            fileInput.setText("");
         }
     }
 
